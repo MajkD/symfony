110e1f809e7ddaea9827cb4e5550739bf7433e2f
Mikael Danielsson <mikael.danielsson@zalando.de>
2017-11-02 14:11:58 +0100

    - persistency with local db

--- intro/src/AppBundle/Controller/DefaultController.php
+++ intro/src/AppBundle/Controller/DefaultController.php
@@ -5,9 +5,48 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Product;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/create_product")
+     */
+    public function createAction(Request $request)
+    {
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to your action: createAction(EntityManagerInterface $em)
+        $em = $this->getDoctrine()->getManager();
+
+        $name = 'keyboard';
+        if ($request->query->has('name')){
+            $name = $request->query->get('name');
+        }
+        $price = 0;
+        if ($request->query->has('price')){
+            $price = $request->query->get('price');
+        }
+        $desc = 'default';
+        if ($request->query->has('desc')){
+            $desc = $request->query->get('desc');
+        }
+
+        $product = new Product();
+        $product->setName($name);
+        $product->setPrice($price);
+        $product->setDescription($desc);
+
+        // tells Doctrine you want to (eventually) save the Product (no queries yet)
+        $em->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $em->flush();
+
+        return new Response('Saved new product with id '.$product->getId());
+    }
+
     /**
      * @Route("/", name="homepage")
      */
---
+++ intro/src/AppBundle/Entity/Product.php
@@ -0,0 +1,43 @@
+<?php
+
+// src/AppBundle/Entity/Product.php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+* @ORM\Entity
+* @ORM\Table(name="product")
+*/
+class Product
+{
+  public function setName($name) { $this->name = $name; }
+  public function setPrice($price) { $this->price = $price; }
+  public function setDescription($description) { $this->description = $description; }
+  public function getName() { return $this->name; }
+  public function getPrice() { return $this->price; }
+  public function getDescription() { return $this->description; }
+  public function getId() { return $this->id; }
+
+  /**
+   * @ORM\Column(type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  private $id;
+
+  /**
+   * @ORM\Column(type="string", length=100)
+   */
+  private $name;
+
+  /**
+   * @ORM\Column(type="decimal", scale=2)
+   */
+  private $price;
+
+  /**
+   * @ORM\Column(type="text")
+   */
+  private $description;
+}
